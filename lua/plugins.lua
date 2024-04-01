cdfa0605e0a33eb4317e0d7eaea82440c37950ca
schockcm <christopher.schock@state.co.us>
2024-04-01 16:53:38 -0600

    added telescope and plenary

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,6 +28,14 @@ require("lazy").setup({
         lazy = false,
     },
     {
+        -- Requred for telescope
+        "nvim-lua/plenary.nvim",
+    },
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.6",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         config = function()
             require("nvim-treesitter.configs").setup({
